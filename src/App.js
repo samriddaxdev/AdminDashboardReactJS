e6250f912695999cd82cd56a84f4cf055b9081e7
Samridda Paudel <84239594+samriddaxdev@users.noreply.github.com>
2024-07-04 18:59:01 +0545

    project import and export made successfull with different components

--- src/App.js
+++ src/App.js
@@ -4,6 +4,25 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { FiSettings } from 'react-icons/fi';
 import { TooltipComponent } from '@syncfusion/ej2-react-popups';
 
+import { Navbar, Footer, Sidebar, ThemeSettings } from './components';
+import {
+  Ecommerce,
+  Orders,
+  Employees,
+  Customers,
+  Calendar,
+  Stacked,
+  Pyramid,
+  Kanban,
+  Area,
+  Bar,
+  Pie,
+  Financial,
+  ColorPicker,
+  ColorMapping,
+  Editor,
+} from './pages';
+
 import './App.css';
 
 const App = () => {
